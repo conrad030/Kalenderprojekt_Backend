a30014808c3d3256c999306328e745c97a457143
conrad030 <conrad.felgentreff@web.de>
2021-11-15 12:15:30 +0100

    Added server and database connection

---
+++ Server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const mysql = require("mysql2")
+const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = mysql.createConnection({
+    port: process.env.DATABASE_PORT,
+    host: 'localhost',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log("Error bei DB: " + err)
+    } else {
+        db.query("select 1 + 1")
+        console.log("MySQL connected")
+    }
+})
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log("Server started on port " + port)
+})
