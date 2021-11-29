70beba41310285f449c143a37f6a466cc77e6a24
Yakup <ykohne@gmail.com>
2021-11-29 13:54:02 +0100

    structure

---
+++ src/routes/usersRouter.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+// User signup
+router.post("/signup", (res, req) => {});
+
+// User login
+router.post("/login", (res, req) => {});
+
+//TODO: Missing CRUDs etc.
