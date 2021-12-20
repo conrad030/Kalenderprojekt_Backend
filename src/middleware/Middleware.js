446ad03658e2e324e88e20a048b8d85cd7b420cb
conrad030 <conrad.felgentreff@web.de>
2021-12-20 15:19:37 +0100

    Added middleware

---
+++ src/middleware/Middleware.js
@@ -0,0 +1,23 @@
+function checkAuthentication(req, res, next) {
+  //If user is authenticated
+  console.log(req.session);
+  if (req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ message: "unauthenticated" });
+  }
+}
+
+function checkAuthorization(req, res, next) {
+  //If user is authorized
+  if (req.session.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: "Not authorized" });
+  }
+}
+
+module.exports = {
+  checkAuthentication,
+  checkAuthorization,
+};
