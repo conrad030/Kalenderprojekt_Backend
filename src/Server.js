9941dd4a2a3136ddc65e661aebc042f36c9185b0
Yakup <ykohne@gmail.com>
2022-01-10 16:59:01 +0100

    cors fix 2

--- src/Server.js
+++ src/Server.js
@@ -63,7 +63,14 @@ app.use("/users", userRouter);
 //   origin: `http://localhost:${process.env.PORT}`,
 // };
 
-app.use(cors());
+app.use(cors({
+    exposedHeaders: ["Authorization"]
+}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
 const port = process.env.PORT;
 app.listen(port, () => {
