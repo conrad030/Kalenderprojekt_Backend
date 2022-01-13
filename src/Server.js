26962dc559b8fee7e0b5719dc0b291e4cc0662cd
conrad030 <conrad.felgentreff@web.de>
2022-01-13 19:40:24 +0100

    cors test

--- src/Server.js
+++ src/Server.js
@@ -14,7 +14,8 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
