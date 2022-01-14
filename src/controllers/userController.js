b095a0c448b4be3bed29c2ed111e904742ecc45f
conrad030 <conrad.felgentreff@web.de>
2022-01-14 17:12:52 +0100

    Regenerate session cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,9 +16,11 @@ exports.login = async function (req, res) {
   var [username, password] = credentials.split(":");
   try {
     let { userId, isAdmin } = await userService.login(username, password);
-    req.session.userId = userId;
-    req.session.isAdmin = isAdmin;
-    res.status(200).json({ message: "User successfully logged in" });
+    req.session.regenerate(function (err) {
+      req.session.userId = userId;
+      req.session.isAdmin = isAdmin;
+      res.status(200).json({ message: "User successfully logged in" });
+    });
   } catch (error) {
     console.log(error.message);
     res.status(404).json({ message: "Bad input" });
